de21be54b4140cfce1e8fba3b87ce98435af997e
Moying_Thor <68772282+Moying-Thor@users.noreply.github.com>
2023-07-14 23:21:52 +0800

    Add files via upload

--- org.eclipse.acme.sacm.diagram.assurancecase/src/assuranceCase/diagram/sheet/util/ImportACHandler.java
+++ org.eclipse.acme.sacm.diagram.assurancecase/src/assuranceCase/diagram/sheet/util/ImportACHandler.java
@@ -8,6 +8,8 @@
  ******************************************************************************/
 package assuranceCase.diagram.sheet.util;
 
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -22,8 +24,12 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
 import org.eclipse.epsilon.emc.spreadsheets.SpreadsheetModel;
+import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.gef.EditPart;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -33,6 +39,8 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 import acme.diagram.util.EditingDomainUtil;
 import acme.diagram.util.ModelElementFeatureUtil;
+import acme.execution.evl.ACMEEVLValidator;
+import acme.execution.evl.EVLUtilityMethods;
 import acme.execution.util.UtilityMethods;
 import assuranceCase.AssuranceCasePackage;
 import assuranceCase.AssuranceCase_Package;
@@ -62,32 +70,125 @@ public class ImportACHandler implements IHandler {
 			return null;
 		}
 		AssuranceCasePackage assuranceCasePackage = null;
-		
 //		Use different models for different extensions
-//		##########################################################
+//		#######################################################################################################
 		//create EMF model with target location
-		try {
-			EmfModel emfmodel = UtilityMethods.createAndLoadAnEmfModel(path, path, path, path, path);
-		} catch (EolModelLoadingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//Identify the suffix name of the file.
+//		Resource resource = resourceSet.getResource(URI.createFileURI(path), true);
+		SpreadsheetModel spreadsheetModel = null;
+		EmfModel emfmodel = null;
+		if (path.substring(path.length()-5).contains(".xlsx")) {
+//---------------------------------E-----------------------------------------
+			try {
+			spreadsheetModel = UtilityMethods.createAndLoadSpreadsheetModel(
+					path,
+					"E",
+					"readOnLoad",
+					"storeOnDisposal");
+			System.out.println("E ok!");
+//---------------------------------M-----------------------------------------
+			AssuranceCase_Package.eINSTANCE.eClass();
+			Registry reg = Registry.INSTANCE;
+			Map<String, Object> m = reg.getExtensionToFactoryMap();
+			m.put("assurancecase", new XMIResourceFactoryImpl());
+			ResourceSet resourceSet = new ResourceSetImpl();
+			Resource resource = resourceSet.getResource(URI.createFileURI("C:\\Users\\yangr\\Git\\acme-new\\org.eclipse.acme.diagram.extension.execution\\files\\files2gsn.model"), true);
+			try {
+				emfmodel = UtilityMethods.loadInMemoryEMFModel(
+						"M",
+						"readOnLoad",
+						"storeOnDisposal",
+						resource,
+						"http://omg.sacm/2.0/assurancecase",
+						"http://omg.sacm/2.0/argumentation",
+						"http://omg.sacm/2.0/base",
+						"http://org.eclipse.acme/1.0/gsn");
+			} catch (EolModelLoadingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("M ok!");
+//----------------------------files2gsn.eol---------------------------------------
+			//load Excel model with location
+			ArrayList<IModel> list = new ArrayList();
+			list.add(emfmodel);
+			list.add(spreadsheetModel);
+			try {
+				UtilityMethods.doEOLTransformation(list, EVLUtilityMethods.getScriptURIfiles2gsn().toString());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			
+//			Resource eolresource = resourceSet.getResource(URI.createFileURI("C:\\Users\\yangr\\Git\\acme-new\\org.eclipse.acme.diagram.extension.execution\\files\\files2gsn.eol"), true);
+//			
+//			InMemoryEmfModel model = new InMemoryEmfModel(eolresource);
+//			model.setName("M");
+//			EolModule module = new EolModule();
+//			module.parse();
+//			module.getContext().getModelRepository().addModel(spreadsheetModel);
+//			module.getContext().getModelRepository().addModel(emfmodel);
+//			module.execute();
+//			resource.save(...);
+			System.out.println("EOL ok!");
+//----------------------------files2gsn.eol---------------------------------------
+			
+//				System.out.println(spreadsheetModel);
+//			ACMEEVLValidator validator = new ACMEEVLValidator();
+//			try {
+//				validator.initialise(EVLUtilityMethods.getScriptURIfiles2gsn(), "M", "org.eclipse.acme.sacm.diagram.assurancecase");
+//			} catch (URISyntaxException e) {
+//				e.printStackTrace();
+//			}
+			
+			
+//				FileDialog eolfileDialog = new FileDialog(Display.getCurrent().getActiveShell());
+//				eolfileDialog.setText("Locate .eol file");
+//				String eolpath = eolfileDialog.open();
+//				if (eolpath == null) {
+//					return null;
+//				}
+//				System.out.println(eolpath);
+//				
+//				
+//				
+//				try {
+//					ArrayList<IModel> list = new ArrayList();
+//					list.add(spreadsheetModel);
+//					list.add(emfmodel);
+//					UtilityMethods.doEOLTransformation(list, eolpath);
+//					System.out.println("eol over!");
+//				} catch (Exception e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//				
+//				
+			} catch (EolModelLoadingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (EolRuntimeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} else if (path.substring(path.length()-5).contains(".xml")) {
+			
+		} else if (path.substring(path.length()-5).contains(".csv")) {
+			
 		}
-		//load Excel model with location 
-		try {
-			SpreadsheetModel spreadsheetModel = UtilityMethods.createAndLoadSpreadsheetModel(path, path, path, path);
-		} catch (EolModelLoadingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		
 //		perform transformation
 //		UtilityMethods.doEOLTransformation(null, path);
 //		emfmodel = 
 //				Resource resource = resourceSet.getResource(URI.createFileURI(path), true);
 //		AssuranceCasePackage assuranceCasePackage = (AssuranceCasePackage) resource.getContents().get(0);
-//		##########################################################
-		
+//		#######################################################################################################
+		path = "C:\\Users\\yangr\\Git\\acme-new\\org.eclipse.acme.diagram.extension.execution\\files\\files2gsn.model";
 		//register AssuranceCasePackage metamodel
+		System.out.println(path);
 		AssuranceCase_Package.eINSTANCE.eClass();
 		Registry reg = Registry.INSTANCE;
 		Map<String, Object> m = reg.getExtensionToFactoryMap();
@@ -97,6 +198,7 @@ public class ImportACHandler implements IHandler {
 		
 		Resource resource = resourceSet.getResource(URI.createFileURI(path), true);
 		assuranceCasePackage = (AssuranceCasePackage) resource.getContents().get(0);
+		
 		
 		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
 		if (selection != null && selection instanceof IStructuredSelection) {
